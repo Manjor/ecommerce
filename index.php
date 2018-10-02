ab5c9884b6f17981ce7ee55ac84785a191065dd8
Manoel <manoeltavares.jr@gmail.com>
2018-10-02 10:55:08 -0300

    Criacao do CRUD Usuarioas

--- index.php
+++ index.php
@@ -50,8 +50,64 @@ $app->post('/admin/login', function(){
 });
 $app->get('/admin/logout',function(){
 	User::logout();
-	header("Location: /admin/login");
+	header("Location: /admin/login/");
+	
 });
+
+
+$app->get("/admin/users", function() {
+	
+	
+
+	$users = User::listAll();
+	$page = new PageAdmin();
+	$page->setTpl("users", array(
+	   "users"=>$users
+   ));
+});
+$app->get("/admin/users/create", function() {
+	
+	$page = new PageAdmin();
+	$page->setTpl("users-create");
+});
+$app->get("/admin/users/:iduser/delete", function($iduser) {
+	
+	$user = new User();
+	$user->get((int)$iduser);
+	$user->delete();
+	header("Location: /admin/users");
+   exit;
+});
+$app->get("/admin/users/:iduser", function($iduser) {
+	
+	$user = new User();
+	$user->get((int)$iduser);
+	$page = new PageAdmin();
+	$page->setTpl("users-update", array(
+	   "user"=>$user->getValues()
+   ));
+});
+$app->post("/admin/users/create", function() {
+	
+	$user = new User();
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+	$user->setData($_POST);
+	$user->save();
+	header("Location: /admin/users");
+   exit;
+});
+$app->post("/admin/users/:iduser", function($iduser) {
+
+	$user = new User();
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+	$user->get((int)$iduser);
+	$user->setData($_POST);
+	$user->update();	
+	header("Location: /admin/users");
+   exit;
+});
+
+
 
 $app->run();
 
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -10,7 +10,7 @@ class User extends Model{
 
 
     const SESSION = "User";
-    public function login($login, $password){
+    public static function login($login, $password){
 
 
         $sql = new Sql();
@@ -28,10 +28,10 @@ class User extends Model{
         if(password_verify($password, $data["despassword"]) === true){
 
             $user = new User();
-            $user->setiduser($data);
+            $user->setData($data);
 
             $_SESSION[User::SESSION] = $user->getValues();
-
+            
             var_dump($user);
             
             return $user;
@@ -65,12 +65,61 @@ class User extends Model{
     public static function logout()
     {
         $_SESSION[User::SESSION] = NULL;
+        header("Location: /admin/login");
     }
 
+    public static function listAll()
+	{
+ 		$sql = new Sql();
+ 		return $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson");
+ 	}
+
+    public function save()
+	{
+ 		$sql = new Sql();
+ 		$results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+			":desperson"=>$this->getdesperson(),
+			":deslogin"=>$this->getdeslogin(),
+			":despassword"=>$this->getdespassword(),
+			":desemail"=>$this->getdesemail(),
+			":nrphone"=>$this->getnrphone(),
+			":inadmin"=>$this->getinadmin()
+		));
+ 		$this->setData($results[0]);
+ 	}
+
+    public function get($iduser)
+	{
+ 		$sql = new Sql();
+ 		$results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = :iduser", array(
+			":iduser"=>$iduser
+		));
+ 		$this->setData($results[0]);
+ 	}
 
 
+    public function update()
+	{
+ 		$sql = new Sql();
+ 		$results = $sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+			":iduser"=>$this->getiduser(),
+			":desperson"=>$this->getdesperson(),
+			":deslogin"=>$this->getdeslogin(),
+			":despassword"=>$this->getdespassword(),
+			":desemail"=>$this->getdesemail(),
+			":nrphone"=>$this->getnrphone(),
+			":inadmin"=>$this->getinadmin()
+		));
+ 		$this->setData($results[0]);		
+ 	}
 
-
+     public function delete()
+	{
+ 		$sql = new Sql();
+ 		$sql->query("CALL sp_users_delete(:iduser)", array(
+			":iduser"=>$this->getiduser()
+		));
+ 	}
 
 
 }
